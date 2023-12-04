c457771c396673ebe2daf8f65096274780a3c7e6
miftakhul huda <huda76998@gmail.com>
2023-12-04 10:36:15 +0700

    navbar tgl 4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,19 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 	<!-- Font Awesome Icons -->
+	
+	<link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/feather-icons/dist/feather.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 	<link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
@@ -44,7 +57,13 @@
 	<script src="{{asset('js/adminlte.min.js')}}"></script>
 	<!-- Sweetalert -->
 	<script src="{{asset('js/sweetalert.min.js')}}"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+
+    <script src="{{ asset('assets/js/script.js') }}"></script>
 	@yield('addJavascript')
 </body>
 
