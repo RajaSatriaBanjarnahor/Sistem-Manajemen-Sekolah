50c61d0b6c21da8871e5d81a9f08f1de9bdfb0b8
miftakhul huda <huda76998@gmail.com>
2023-12-19 13:23:14 +0700

    update huda tgl 19.

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Route; // Pastikan mengimpor Route
+use Illuminate\Support\Facades\Auth; // Pastikan mengimpor Auth
 use App\Http\Controllers\TabelUserController;
 use App\Http\Controllers\TabelStudentController;
 use App\Http\Controllers\TabelAdminController;
 use App\Http\Controllers\TabelTeacherController;
 
 
-Route::get('/', function() {
+
+Route::get('/', function () {
     return redirect(route('login'));
 });
-Route::get('/starter', function() {
+Route::get('/starter', function () {
     return view('starter');
 });
-Route::get('/TabelUserAdd', function() {
+Route::get('/TabelUserAdd', function () {
     return view('admin.tabelUserAdd');
 });
-Route::get('/TabelUserEdit', function() {
+Route::get('/TabelUserEdit', function () {
     return view('admin.tabelUserEdit');
 });
 
@@ -45,6 +49,11 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('/TabelUser/update/{id}', [TabelUserController::class, 'update'])->name('UpdateUser');
     Route::get('/TabelUser/delete/{id}', [TabelUserController::class, 'delete'])->name('DeleteUser');
     Route::get('/TabelStudents', [TabelStudentController::class, 'index'])->name('tabelStudents');
+    Route::get('/TabelStudents/edit/{StudentID}', [TabelStudentController::class, 'edit'])->name('EditStudents');
+    Route::post('/TabelStudents/update/{StudentID}', [TabelStudentController::class, 'update'])->name('UpdateStudents');
+    Route::get('/TabelStudents/delete/{StudentID}', [TabelStudentController::class, 'delete'])->name('DeleteStudents');
+    Route::get('/TabelStudents/create', [TabelStudentController::class, 'create'])->name('AddStudents');
+    Route::post('/TabelStudents/store', [TabelStudentController::class, 'store'])->name('StoreStudents');
     Route::get('/TabelAdmins', [TabelAdminController::class, 'index'])->name('tabelAdmins');
     Route::get('/TabelTeachers', [TabelTeacherController::class, 'index'])->name('tabelTeachers');
 });
@@ -57,3 +66,6 @@ Route::group(['middleware' => 'siswa'], function () {
     Route::get('siswa.dashboard', 'SiswaController@dashboard')->name('siswa.dashboard');
 });
 
+Route::group(['middleware' => 'Student'], function () {
+    Route::get('Student.dashboard', 'TabelStudentController@dashboard')->name('Student.dashboard');
+});
